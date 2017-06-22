8e720715d4a56ae524bfe69bb250f07356ac8178
guillelopez22 <guillelopez_13@hotmail.com>
2017-06-22 00:43:48 -0600

    fixed

--- schemas/usuario.js
+++ schemas/usuario.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
 
@@ -14,18 +14,3 @@ var UsuarioSchema = new mongoose.Schema({
 
 UserSchema.plugin(uniqueValidator);
 module.exports = mongoose.model('usuario', UsuarioSchema);
-||||||| merged common ancestors
-=======
-var mongoose = require('mongoose');
-
-var UsuarioSchema = new mongoose.Schema({
-  IdPersonal: String,
-  idOrdenes: [String],
-  usuario : String,
-  contrasena : String,
-  nombre : String,
-  telefono : String
-});
-
-module.exports = mongoose.model('usuario', UsuarioSchema);
->>>>>>> ab25651f8ac8bda8263754e566008ca184619a1a
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var hapi = require('hapi');
 var inert = require('inert');
 var mongoose = require('mongoose');
@@ -39,46 +38,3 @@ server.register([inert, auth], function(err){
 	    console.log('Server running at:', server.info.uri);
 	});
 });
-||||||| merged common ancestors
-=======
-var hapi = require('hapi');
-var inert = require('inert');
-var mongoose = require('mongoose');
-var routes = require('./routes');
-var auth = require('hapi-auth-cookie');
-
-var server = new hapi.Server();
-server.connection({
-    port: ~~process.env.PORT || 8000,
-    routes: {
-      cors: {//cross origin request service: permite rquests de afuera del server
-        credentials: true,
-        origin: ["*"]
-      }
-    }
-});
-
-mongoose.connect('mongodb://admin:admin@ds123361.mlab.com:23361/thespot');
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', function callback() {
-    console.log("Connection with database succeeded.");
-});
-
-server.register([inert, auth], function(err){
-
-  // server.auth.strategy('session', 'cookie', {
-  //   password: 'secretpasswordforencryption',
-  //   cookie: 'angular-scaffold-cookie',
-  //   ttl: 24 * 60 * 60 * 1000, // Set session to 1 day
-  //   isSecure: false
-  // });
-  // routes.endpoints
-	server.route(routes.endpoints);
-
-	server.start(function () {
-	    console.log('Server running at:', server.info.uri);
-	});
-});
->>>>>>> ab25651f8ac8bda8263754e566008ca184619a1a
